c48b04674b799a3e90da7a649277198ce0c5d2c6
unknown <juans.casado@hotmail.com>
2025-02-04 10:39:05 -0400

    requirements without ymls??_next_day_ fix build.sh

--- build.sh
+++ build.sh
@@ -1,18 +1,8 @@
 #!/usr/bin/env bash
 
-# Exit on error
 set -o errexit
 
-# Install dependencies
 pip install -r requirements.txt
 
-# Run migrations
-python manage.py migrate
-
-# Collect static files
 python manage.py collectstatic --noinput
-
-# Crear carpeta de medios si no existe
-if [ ! -d "media" ]; then
-  mkdir media
-fi
+python manage.py migrate
