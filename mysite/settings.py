fd98038b159ee589b77738b176a7b20d4fb75156
unknown <juans.casado@hotmail.com>
2025-02-03 20:36:30 -0400

    requirements without ymls??_3

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'sstatic/'
+STATIC_URL = '/static/'
 if not DEBUG:
   STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
   STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
@@ -136,7 +136,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Load the YAML file
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 # Set MEDIA_ROOT from YAML
 MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
 
