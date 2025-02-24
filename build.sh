f2a55538505a371732212dc467e42a7e2c04d481
unknown <juans.casado@hotmail.com>
2025-02-24 16:16:15 -0400

    primer commit despues de pueblo nue..

--- build.sh
+++ build.sh
@@ -1,8 +1,29 @@
-#!/usr/bin/env bash
+export DJANGO_SUPERUSER_NAME=myadmin
+export DJANGO_SUPERUSER_EMAIL=myadmin@example.com
+export DJANGO_SUPERUSER_PASSWORD=securepassword
+./build.sh#!/usr/bin/env bash
 
 set -o errexit
 
 pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
+#!/bin/sh
+
+# Aplica migraciones
 python manage.py migrate
+
+# Crear superusuario sin entrada manual
+SUPERUSER_NAME=${DJANGO_SUPERUSER_NAME:-admin}
+SUPERUSER_EMAIL=${DJANGO_SUPERUSER_EMAIL:-admin@example.com}
+SUPERUSER_PASSWORD=${DJANGO_SUPERUSER_PASSWORD:-adminpassword}
+
+echo "from django.contrib.auth import get_user_model;
+User = get_user_model();
+if not User.objects.filter(username='$SUPERUSER_NAME').exists():
+    User.objects.create_superuser('$SUPERUSER_NAME', '$SUPERUSER_EMAIL', '$SUPERUSER_PASSWORD')" | python manage.py shell
+
+echo "from django.contrib.auth import get_user_model;
+User = get_user_model();
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'juans.casado@gmail.com', 'adminpassword')" | python manage.py shell
